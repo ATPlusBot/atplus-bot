1a0f2cde4feef8a6edf38703747ed04a983543e8
Shuntaro Honjo <shuntaro.honjo@fxat.co.jp>
2017-11-10 14:12:25 +0900

    Fix authenticator name.

--- index.js
+++ index.js
@@ -6,20 +6,13 @@ const botURL = 'https://atplus-bot.azurewebsites.net';
 const OUTLOOK_CLIENT_ID = process.env.MICROSOFT_APP_ID;
 const OUTLOOK_CLIENT_SECRET = process.env.MICROSOFT_APP_PASSWORD;
 
-// const fs = require('fs');
 const restify = require('restify');
 const builder = require('botbuilder');
 const botauth = require('botauth');
 const OutlookStrategy = require('passport-outlook').Strategy;
-/*
-const https_options = {
-	key: fs.readFileSync('/home/chatbotdev/honjo/ssl/server.key'),
-	certificate: fs.readFileSync('/home/chatbotdev/honjo/ssl/server.crt')
-};
-*/
 
 const port = process.env.port || process.env.PORT || localPort;
-// const server = restify.createServer(https_options);
+
 const server = restify.createServer();
 server.use(restify.plugins.queryParser({mapParams: false}));
 
@@ -28,12 +21,6 @@ const connector = new builder.ChatConnector({
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-/*
-const connector = new builder.ChatConnector({
-	appId: null,
-	appPassword: null
-});
-*/
 server.post('/api/messages', connector.listen());
 
 const bot = new builder.UniversalBot(connector);
@@ -65,18 +52,10 @@ bot.dialog('/', [].concat(
 	},
 	botAuthenticator.authenticate('outlook'),
 	(session, results) => {
-		let user = auth.profile(session, 'outlook');
+		let user = botAuthenticator.profile(session, 'outlook');
 		session.endDialog(`Welcome ${user.displayName}`);
 	}
 ));
-
-/*
-bot.dialog('/', [
-	(session) => {
-		session.send('You said: %s', session.message.text);
-	}
-]);
-*/
 
 server.listen(port);
 
