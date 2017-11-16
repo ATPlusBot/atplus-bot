8cd978c050a668e5a38ead0ea439fbede734f967
Taguchi <yusuke.taguchi@fxat.co.jp>
2017-11-16 18:17:18 +0900

    luistest_taguchi

--- index.js
+++ index.js
@@ -100,8 +100,8 @@ intents
 		// EntityRecognizerを使うと、指定したエンティティの内容を抽出できます。
 		var meeting = builder.EntityRecognizer.findEntity(args.entities, '会議');
 
-		session.send("endIndex = %d, entity = %d, score = %d, startIndex = %d, type = %s.",
-						meeting.endIndex,meeting.entity,meeting.score,meeting,startIndex,meeting.type);
+		var data = JSON.stringify(entity);
+		session.send("data = %s.", data);
 		// 「場所」エンティティが認識できた場合の処理
 		if (meeting) 
 		{
