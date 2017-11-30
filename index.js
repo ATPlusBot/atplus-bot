28f60b5aa5cb0a9b5ec4d2533c5a9e0fdb176e66
Taguchi <yusuke.taguchi@fxat.co.jp>
2017-11-30 11:38:57 +0900

    oauth_luis_test

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
 'use strict';
 
-const restify = require('restify');
-const builder = require('botbuilder');
-
 const port = process.env.port || process.env.PORT || 3978;
+const botURL = 'https://atplus-bot.azurewebsites.net';
+const OUTLOOK_CLIENT_ID = process.env.MICROSOFT_APP_ID;
+const OUTLOOK_CLIENT_SECRET = process.env.MICROSOFT_APP_PASSWORD;
+
+const url = require('url');
+const restify = require('restify');
+const clients = require('restify-clients');
+const builder = require('botbuilder');
+const botauth = require('botauth');
+const OutlookStrategy = require('passport-outlook').Strategy;
+
 const server = restify.createServer();
-server.listen(port);
+server.use(restify.plugins.queryParser({mapParams: false}));
 
 const connector = new builder.ChatConnector({
 appId: process.env.MICROSOFT_APP_ID,
 appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
+server.post('/api/messages', connector.listen());
+
 const bot = new builder.UniversalBot(connector);
 
-server.post('/api/messages', connector.listen());
+const botAuthenticator = new botauth.BotAuthenticator(server, bot, {
+secret: 'something secret',
+baseUrl: botURL,
+scope: [
+'openid',
+'profile',
+'offline_access',
+'https://outlook.office.com/Mail.Read'
+]
+});
+
+botAuthenticator.provider('outlook', (options) => {
+		return new OutlookStrategy(
+				{
+clientID: OUTLOOK_CLIENT_ID,
+clientSecret: OUTLOOK_CLIENT_SECRET,
+callbackURL: options.callbackURL
+},
+(accessToken, refreshToken, profile, done) => {
+profile.acessToken = accessToken;
+profile.refreshToken = refreshToken;
+return done(null, profile);
+}
+);
+		});
+
+
+bot.dialog('/', [].concat(
+			(session, args, next) => {
+			session.send('Hello!');
+			next({});
+			},
+			botAuthenticator.authenticate('outlook'),
+			(session) => {
+			let user = botAuthenticator.profile(session, 'outlook');
+			session.send(`Welcome ${user.displayName}`);
+
+			let u = url.parse('https://outlook.office.com/api/v2.0/me/messages');
+
+			let client = clients.createJsonClient({
+url: url.resolve(u, '/'),
+headers: {
+Authorization: `Bearer ${user.acessToken}` //actual spelling
+}
+});
+			client.get(u.path, (err, req, res, obj) => {
+				if(err) {
+				session.send(`error: ${err}`);
+				} else {
+				session.send(`last mail: ${JSON.stringify(obj.value[0])}`);
+				}
+
+				session.endDialog('session end.');
+				});
+}
+));
+
+server.listen(port);
 
 //=========================================================
 // IntentDialogオブジェクトの用意
@@ -24,11 +91,6 @@ server.post('/api/messages', connector.listen());
 var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/8baa6be4-7058-4a67-987e-ba9fa4f49b85?subscription-key=d9b1431a2da143d7948d551952a757ef&verbose=true&timezoneOffset=0&q=');
 
 bot.recognizer(recognizer);
-
-//状態
-var meetingSts;
-
-
 
 // Main menu
 var menuItems = { 
@@ -42,25 +104,19 @@ item: "no"
 bot.dialog('SetupMeeting', [
 		function (session, args, next) {
 
-		var meeting = builder.EntityRecognizer.findEntity(args.intent.entities, '会議');
-		session.send("intent = SetupMeeting." );
+			var meeting = builder.EntityRecognizer.findEntity(args.intent.entities, '会議');
+			session.send("intent = SetupMeeting." );
 
-		var data2 = JSON.stringify(args);
-		session.send("data = %s.", data2);
-		//var data2 = JSON.stringify(meeting);
-		//session.send("data = %s.", data2);
-		// 「場所」エンティティが認識できた場合の処理
-		if (meeting) 
-		{
-		builder.Prompts.confirm(session, "打ち合わせ調整しますか?(yes or no)");
-		//builder.Prompts.choice(session, "打ち合わせ調整しますか?", menuItems);
-		// city entity detected, continue to next step
-		meetingSts = 'meeting';
-		//next({ response: meeting.entity });
-		}
-		else {
-		session.send("error.");
-		}
+			var data2 = JSON.stringify(args);
+			session.send("data = %s.", data2);
+			// 「場所」エンティティが認識できた場合の処理
+			if (meeting) 
+			{
+				builder.Prompts.confirm(session, "打ち合わせ調整しますか?(yes or no)");
+			}
+			else {
+				session.send("error.");
+			}
 		},
 		function (session, results) {
 			var data2 = JSON.stringify(results);
@@ -85,27 +141,23 @@ bot.dialog('SetupMeeting', [
 		}
 ]) .triggerAction({
 matches: 'SetupMeeting',
-////onInterrupted: function (session) {
-////session.send('Please provide a destination');
-////}
-//confirmPrompt: "This will cancel your request. Are you sure?"
 });
 
 bot.dialog('MeetingSpace', [
 		function (session, args, next) {
 
-		var place = builder.EntityRecognizer.findEntity(args.intent.entities, '場所');
-		session.send("intent = MeetingSpace." );
+			var place = builder.EntityRecognizer.findEntity(args.intent.entities, '場所');
+			session.send("intent = MeetingSpace." );
 
-		// 「場所」エンティティが認識できた場合の処理
-		if (place) 
-		{
-		session.send("場所は %s ですね？.",place.entity);
-		builder.Prompts.choice(session, "Select Menu:", menuItems);
-		// city entity detected, continue to next step
-		session.dialogData.searchType = 'space';
-		//next({ response: meeting.entity });
-		}
+			// 「場所」エンティティが認識できた場合の処理
+			if (place) 
+			{
+				session.send("場所は %s ですね？.",place.entity);
+				builder.Prompts.choice(session, "Select Menu:", menuItems);
+				// city entity detected, continue to next step
+				session.dialogData.searchType = 'space';
+				//next({ response: meeting.entity });
+			}
 		},
 		function (session, results) {
 			if ( results.response.index == 0 ){
@@ -113,14 +165,12 @@ bot.dialog('MeetingSpace', [
 			} else {
 				session.send("調整をおわります!!!%d", results.response.index);
 			}
-			// End
-			//session.endDialog();
 		},
 		function (session, results) {
 			var data2 = JSON.stringify(results);
 			session.send("data = %s.", data2);
 			if ( results.response != 0 ){
-				session.send("%dですね。", results.response);
+				session.send("%d人ですね。", results.response);
 				builder.Prompts.text(session, "どなたが参加しますか?");
 			} else {
 				session.send("調整をおわります!!!%d", results.response);
@@ -135,33 +185,3 @@ bot.dialog('MeetingSpace', [
 matches: 'MeetingSpace',
 });
 
-// IntentDialogオブジェクトを作成
-//var intents = new builder.IntentDialog({
-//recognizers: [recognizer]
-//});
-
-// 会話の処理
-//=========================================================
-
-//初期ダイアログを、intentDialogとして使用する
-//bot.dialog('/', intents);
-//
-//// インテントと処理の結びつけ
-//intents
-//.matches('SetupMeeting', function (session, args) {
-//
-//		// インテントが 'intentA' だったときの処理をここに記述します。
-//
-//		// EntityRecognizerを使うと、指定したエンティティの内容を抽出できます。
-//		var meeting = builder.EntityRecognizer.findEntity(args.entities, '会議');
-//
-//		var data = JSON.stringify(meeting);
-//		session.send("data = %s.", data);
-//		// 「場所」エンティティが認識できた場合の処理
-//		if (meeting) 
-//		{
-//		session.send("打ち合わせ調整しますか?%s.", session.message.text);
-//		}
-//
-//})
-//
