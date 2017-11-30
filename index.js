4fa748706207a2931c5f3e8a6b8b862e5c4f164d
Taguchi <yusuke.taguchi@fxat.co.jp>
2017-11-30 15:08:37 +0900

    oauth_luis_test

--- index.js
+++ index.js
@@ -52,35 +52,35 @@ botAuthenticator.provider('outlook', (options) => {
 	);
 });
 
-bot.dialog('/', [].concat(
-	(session, args, next) => {
-		session.send('Hello!');
-		next({});
-	},
-	botAuthenticator.authenticate('outlook'),
-	(session) => {
-		let user = botAuthenticator.profile(session, 'outlook');
-		session.send(`Welcome ${user.displayName}`);
-
-		let u = url.parse('https://outlook.office.com/api/v2.0/me/messages');
-
-		let client = clients.createJsonClient({
-			url: url.resolve(u, '/'),
-			headers: {
-				Authorization: `Bearer ${user.acessToken}` //actual spelling
-			}
-		});
-		client.get(u.path, (err, req, res, obj) => {
-			if(err) {
-				session.send(`error: ${err}`);
-			} else {
-				session.send(`last mail: ${JSON.stringify(obj.value[0])}`);
-			}
-
-			session.endDialog('session end.');
-		});
-	}
-));
+//bot.dialog('/', [].concat(
+//	(session, args, next) => {
+//		session.send('Hello!');
+//		next({});
+//	},
+//	botAuthenticator.authenticate('outlook'),
+//	(session) => {
+//		let user = botAuthenticator.profile(session, 'outlook');
+//		session.send(`Welcome ${user.displayName}`);
+//
+//		let u = url.parse('https://outlook.office.com/api/v2.0/me/messages');
+//
+//		let client = clients.createJsonClient({
+//			url: url.resolve(u, '/'),
+//			headers: {
+//				Authorization: `Bearer ${user.acessToken}` //actual spelling
+//			}
+//		});
+//		client.get(u.path, (err, req, res, obj) => {
+//			if(err) {
+//				session.send(`error: ${err}`);
+//			} else {
+//				session.send(`last mail: ${JSON.stringify(obj.value[0])}`);
+//			}
+//
+//			session.endDialog('session end.');
+//		});
+//	}
+//));
 
 server.listen(port);
 
@@ -127,27 +127,27 @@ bot.dialog('SetupMeeting', [
 				session.send("調整しましょう!!!");
 
 
-//	botAuthenticator.authenticate('outlook');
-//		let user = botAuthenticator.profile(session, 'outlook');
-//		session.send(`Welcome ${user.displayName}`);
-//
-//		let u = url.parse('https://outlook.office.com/api/v2.0/me/messages');
-//
-//		let client = clients.createJsonClient({
-//			url: url.resolve(u, '/'),
-//			headers: {
-//				Authorization: `Bearer ${user.acessToken}` //actual spelling
-//			}
-//		});
-//		client.get(u.path, (err, req, res, obj) => {
-//			if(err) {
-//				session.send(`error: ${err}`);
-//			} else {
-//				session.send(`last mail: ${JSON.stringify(obj.value[0])}`);
-//			}
-//
-//			session.endDialog('session end.');
-//		});
+		botAuthenticator.authenticate('outlook');
+		let user = botAuthenticator.profile(session, 'outlook');
+		session.send(`Welcome ${user.displayName}`);
+
+		let u = url.parse('https://outlook.office.com/api/v2.0/me/messages');
+
+		let client = clients.createJsonClient({
+			url: url.resolve(u, '/'),
+			headers: {
+				Authorization: `Bearer ${user.acessToken}` //actual spelling
+			}
+		});
+		client.get(u.path, (err, req, res, obj) => {
+			if(err) {
+				session.send(`error: ${err}`);
+			} else {
+				session.send(`last mail: ${JSON.stringify(obj.value[0])}`);
+			}
+
+			session.endDialog('session end.');
+		});
 
 
 				session.send("場所はどこにしますか？");
